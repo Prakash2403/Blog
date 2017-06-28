e6c4e29a949be83a360e16c64956edda87e1beca
= <201551009@iiitvadodara.ac.in>
2017-06-29 03:31:32 +0530

    - Added gitignore - Patched archive problem

--- aboutme/admin.py
+++ aboutme/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from aboutme.models import AboutMe
+from aboutme.models import AboutMe, ContactMe
 
 admin.site.register(AboutMe)
+admin.site.register(ContactMe)
--- archive/views.py
+++ archive/views.py
@@ -16,7 +16,7 @@ def index(request):
     for year in range(curr_year, start_year, -1):
         archive_post_year = OrderedDict()
         for i in range(12, 0, -1):
-            post = posts.filter(datetime__month=i).filter(datetime__year=year)
+            post = posts.filter(datetime__month=i).filter(datetime__year=year).filter(draft=False)
             if post:
                 archive_post_year[str(calendar.month_name[i])] = post
         archive_posts[str(year)] = archive_post_year
